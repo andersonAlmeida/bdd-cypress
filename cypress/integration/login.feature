5a1017e3f1d3acc6bf800a6f8155350debb14419
Anderson Almeida <anderson.almeida@pmweb.com.br>
2020-05-05 20:26:00 -0300

    atualizacao apresentacao

--- cypress/integration/login.feature
+++ cypress/integration/login.feature
@@ -1,16 +1,18 @@
 Feature: Efetuar login no portal do aluno
 
   Scenario: Login inválido
-    Given o usuário insere os dados de login errados
-    When apertar o botão para entrar no portal
-    Then devo visualizar o aviso de Usuário inválido ou senha incorreta.
+    Given o usuário acessa a página de login
+    When o usuário insere os dados de login errados
+      And aperta o botão para entrar no portal
+    Then deve visualizar o aviso de Usuário inválido ou senha incorreta.
 
   Scenario: Recuperar senha
     Given o usuário acessa a página de login
     When apertar o link Esqueceu a senha?
-    Then devo visualizar o formulário de recuperação de senha
+    Then deve visualizar o formulário de recuperação de senha
 
   Scenario: Login válido
     Given o usuário acessa a página de login
-    When o usuário insere os dados de login válidos e aperta o botão para entrar no portal
+    When o usuário insere os dados de login válidos
+      And aperta o botão para entrar no portal
     Then o usuário é redirecionado para a página principal
--- cypress/support/pageobjects/login.page.js
+++ cypress/support/pageobjects/login.page.js
@@ -1,42 +1,54 @@
 /// <reference types="Cypress" />
 
-import LoginElements from '../elements/login.elements'
-const { botaoLogin, inputUsuario, inputSenha, msgErro, linkRecuperarSenha, formRecuperarSenha, inputAlunoId } = new LoginElements
-const url = Cypress.config("baseUrl")
+import LoginElements from "../elements/login.elements";
+const {
+  botaoLogin,
+  inputUsuario,
+  inputSenha,
+  msgErro,
+  linkRecuperarSenha,
+  formRecuperarSenha,
+  inputAlunoId,
+} = new LoginElements();
+const url = Cypress.config("baseUrl");
 
 class LoginPage {
-    // Acessa o link que será testado
-    acessarPaginaLogin() {
-        cy.visit(url)
-    }
+  // Acessa o link que será testado
+  acessarPaginaLogin() {
+    cy.visit(url);
+  }
 
-    // Tenta executar o login
-    efetuaLogin(user, password) {
-        cy.get(inputUsuario()).then($input => {
-            cy.get($input).type(user)
-        })
-        cy.get(inputSenha()).then($input => {
-            cy.get($input).type(password)
-        })
-        cy.get(botaoLogin()).click()
-    }
+  // preenche os campos de login
+  insereDadosLogin(user, password) {
+    cy.get(inputUsuario()).then(($input) => {
+      cy.get($input).type(user);
+    });
+    cy.get(inputSenha()).then(($input) => {
+      cy.get($input).type(password);
+    });
+  }
 
-    // Verifica se abriu exibiu mensagem de login inválido"
-    visualizarMsgErro() {
-        cy.get(msgErro()).should('contain', 'Usuário inválido ou senha incorreta.')
-    }
+  // submete o formulário de login
+  clicaLogar() {
+    cy.get(botaoLogin()).click();
+  }
 
-    clicaRecuperarSenha() {
-        cy.get(linkRecuperarSenha()).click()
-    }
+  // Verifica se abriu exibiu mensagem de login inválido"
+  visualizarMsgErro() {
+    cy.get(msgErro()).should("contain", "Usuário inválido ou senha incorreta.");
+  }
 
-    visualizarFormRecuperarSenha() {
-        cy.get(formRecuperarSenha()).should('be.visible')
-    }
+  clicaRecuperarSenha() {
+    cy.get(linkRecuperarSenha()).click();
+  }
 
-    verificaPaginaLogado() {
-        cy.get(inputAlunoId()).should('have.value', Cypress.env('id'))
-    }
+  visualizarFormRecuperarSenha() {
+    cy.get(formRecuperarSenha()).should("be.visible");
+  }
+
+  verificaPaginaLogado() {
+    cy.get(inputAlunoId()).should("have.value", Cypress.env("id"));
+  }
 }
 
 export default LoginPage;
